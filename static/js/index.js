69ec883814286173d0a9e5485b5b000a12801d34
Raha Moosavi <rahamoosavi1378@gmail.com>
2020-11-21 20:33:34 +0100

    fix bug hidden span and navBar

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,25 @@
 let select_navBar = document.getElementById('select_navBar');
-let navBar = document.querySelectorAll('#navBar span')
+let navBar = document.getElementById('navBar')
+let navBar_span = document.querySelectorAll('#navBar span')
 let logo_text = document.getElementById('logo_text');
 
-console.log(navBar);
+let toggle_select_navBar = 0;
 select_navBar.addEventListener('click', () => {
-    for (const navBarKey in navBar) {
-        navBar[navBarKey].classList.toggle('hidden');
-        console.log(navBarKey)
+    if (toggle_select_navBar === 0) {
+        for (let i = 0; i < navBar_span.length; i++) {
+            navBar_span[i].classList.toggle('hidden');
+        }
+        navBar.style.width = '3%';
+        toggle_select_navBar++;
+    } else if (toggle_select_navBar === 1) {
+        for (let i = 0; i < navBar_span.length; i++) {
+            setTimeout(()=> {
+                navBar_span[i].classList.toggle('hidden');
+            },500)
+        }
+        navBar.style.width = '15%';
+        toggle_select_navBar--;
+    } else {
+        console.log('error')
     }
 });
