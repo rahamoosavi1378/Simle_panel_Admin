826d4072386334ef4dedcfb642235075c3201a24
Raha Moosavi <rahamoosavi1378@gmail.com>
2020-12-01 19:19:16 +0100

    fix bug navBar 02

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,7 @@ let navBar_span = document.querySelectorAll('#navBar span');
 let navBar_a = document.querySelectorAll('#navBar a');
 let logo_text = document.getElementById('logo_text');
 let header_menu = document.getElementById('header_menu');
+let content = document.getElementById('content');
 
 let toggle_select_navBar = 0;
 select_navBar.addEventListener('click', () => {
@@ -12,8 +13,14 @@ select_navBar.addEventListener('click', () => {
             navBar_span[i].classList.toggle('hidden');
             // navBar_a[i]. -> :hover padding-right : 10px
         }
-        navBar.style.width = '3%';
-        logo_text.style.width = '3%';
+        navBar.style.width = '40px';
+        content.style.width = '97%';
+        logo_text.style.width = '40px';
+        logo_text.innerText = '';
+        setTimeout(() => {
+            logo_text.classList.toggle('fa');
+            logo_text.classList.toggle('fa-cubes');
+        }, 500)
         header_menu.style.width = '97%';
         toggle_select_navBar++;
     } else if (toggle_select_navBar === 1) {
@@ -23,7 +30,13 @@ select_navBar.addEventListener('click', () => {
             }, 500)
         }
         navBar.style.width = '15%';
+        content.style.width = '85%';
         logo_text.style.width = '15%';
+        logo_text.classList.toggle('fa');
+        logo_text.classList.toggle('fa-cubes');
+        setTimeout(() => {
+            logo_text.innerText = 'PAdmin';
+        }, 500)
         header_menu.style.width = '85%';
         toggle_select_navBar--;
     } else {
